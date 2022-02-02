7ba51e0c74bc0cd00a2ad909f023abd3a95a70b1
Connie Tran Hedberg <connietranhedberg@gmail.com>
2022-02-02 13:03:49 +0100

    Retrack version with passed tests and .gitignore

--- src/main/java/se/sensera/banking/Implementation/AccountServiceImpl.java
+++ src/main/java/se/sensera/banking/Implementation/AccountServiceImpl.java
@@ -4,8 +4,13 @@ import se.sensera.banking.*;
 import se.sensera.banking.exceptions.Activity;
 import se.sensera.banking.exceptions.UseException;
 import se.sensera.banking.exceptions.UseExceptionType;
+import se.sensera.banking.utils.ListUtils;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 import java.util.function.Consumer;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 
@@ -60,17 +65,25 @@ public class AccountServiceImpl implements AccountService {
 
     @Override
     public Account addUserToAccount(String userId, String accountId, String userIdToBeAssigned) throws UseException {
+        if (accountsRepository.getEntityById(accountId).isEmpty()) {
+            throw new UseException(Activity.UPDATE_ACCOUNT, UseExceptionType.NOT_FOUND, "Account does not exist");
+        }
         Account accountUser = accountsRepository.getEntityById(accountId).get(); //Nuvarande account
         User currentUser = usersRepository.getEntityById(userId).get(); //Nuvarande user
         User newUser = usersRepository.getEntityById(userIdToBeAssigned).get(); //ny user
-
-        if (!accountUser.getOwner().getId().equals(currentUser.getId())) {//Test rad 94. Ska inte kunna lägga till användare för att man inte är ägare
+        Account account = accountsRepository.getEntityById(accountId).get();
+        if (!accountUser.getOwner().getId().equals(userId)) {//Test rad 94. Ska inte kunna lägga till användare för att man inte är ägare
             throw new UseException(Activity.UPDATE_ACCOUNT, UseExceptionType.NOT_OWNER, "Not owner");
         }
-        if (accountsRepository.getEntityById(accountId).get().getOwner().getId().equals(userIdToBeAssigned)){//Test rad 80, kan inte lägga till ägare som användare
+        if (!account.isActive()) {
+            throw new UseException(Activity.UPDATE_ACCOUNT, UseExceptionType.ACCOUNT_NOT_ACTIVE, "Account not active"); //account inte aktivt
+        }
+        if (account.getOwner().getId().equals(userIdToBeAssigned)) {//Test rad 80, kan inte lägga till ägare som användare
             throw new UseException(Activity.UPDATE_ACCOUNT, UseExceptionType.CANNOT_ADD_OWNER_AS_USER, "Cannot add owner as user");
         }
-        //HITTAT KONTOT
+        if (account.getUsers().anyMatch(x -> x.getId().equals(userIdToBeAssigned))) {
+            throw new UseException(Activity.UPDATE_ACCOUNT, UseExceptionType.USER_ALREADY_ASSIGNED_TO_THIS_ACCOUNT, "Already assigned");
+        }
         usersRepository.save(newUser);
         accountsRepository.save(accountUser);
         accountUser.addUser(newUser);
@@ -79,7 +92,17 @@ public class AccountServiceImpl implements AccountService {
 
     @Override
     public Account removeUserFromAccount(String userId, String accountId, String userIdToBeAssigned) throws UseException {
-        return null;
+        Account account = accountsRepository.getEntityById(accountId).get();
+        User currUser = usersRepository.getEntityById(userIdToBeAssigned).get();
+        if (!account.getOwner().getId().equals(userId)) {//Test rad 94. Ska inte kunna lägga till användare för att man inte är ägare
+            throw new UseException(Activity.UPDATE_ACCOUNT, UseExceptionType.NOT_OWNER, "Not owner");
+        }
+        if (account.getUsers().noneMatch(x -> x.getId().equals(userIdToBeAssigned))) {
+            throw new UseException(Activity.UPDATE_ACCOUNT, UseExceptionType.USER_NOT_ASSIGNED_TO_THIS_ACCOUNT, "Are you lost?");
+        }
+        account.removeUser(currUser);
+        accountsRepository.save(account);
+        return account;
     }
 
     @Override
@@ -92,7 +115,7 @@ public class AccountServiceImpl implements AccountService {
         Account account = accountsRepository.getEntityById(accountId).get();
         User userAccount = usersRepository.getEntityById(userId).get();
 
-        if (!account.getOwner().equals(userAccount)){
+        if (!account.getOwner().equals(userAccount)) {
             throw new UseException(Activity.INACTIVATE_ACCOUNT, UseExceptionType.NOT_OWNER, "Not Owner");
         }
         if (!account.isActive()) {
@@ -105,6 +128,25 @@ public class AccountServiceImpl implements AccountService {
 
     @Override
     public Stream<Account> findAccounts(String searchValue, String userId, Integer pageNumber, Integer pageSize, SortOrder sortOrder) throws UseException {
-        return accountsRepository.all();
+        List<Account> listOfAccounts = new ArrayList<>();
+        // Vid en slagning kommer en searchValue som kan vara namn/Personnummer
+        // Gå igenom repot med konton
+        // Baserat på searchValue vill vi filtrera ut dem
+        // Beroende på hur användaren vill ha det filtrerat, sorterar vi dem utifrån sortOrder
+        //Hämta ut Lisas account
+        // spara till lista
+
+        listOfAccounts = accountsRepository.all()
+                .filter(x -> x.getName().toLowerCase().contains(searchValue.toLowerCase()))
+                .map(x -> {
+                    if(x.getId().contains(userId)){
+                        return x;
+                    }else
+                        return null;
+                })
+                .sorted(Comparator.comparing(Account::getName))
+                .collect(Collectors.toList());
+
+        return ListUtils.applyPage(listOfAccounts.stream(), pageNumber, pageSize);
     }
 }
